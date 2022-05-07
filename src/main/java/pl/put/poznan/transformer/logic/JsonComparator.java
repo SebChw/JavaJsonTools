d748e5718560d388952ffeab4cf748ea6ab03d80
kapiblue <kapiblue@outlook.com>
2022-05-07 11:27:15 +0200

    Create JsonComparator

---
+++ src/main/java/pl/put/poznan/transformer/logic/JsonComparator.java
@@ -0,0 +1,11 @@
+package pl.put.poznan.transformer.logic;
+
+public class JsonComparator implements Comparator{
+
+    private JsonFormatter formatter;
+
+    @Override
+    public void compare(String text1, String text2) {
+
+    }
+}
