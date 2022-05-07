998be49c8d7d355225994807a6a5ce6a7dcb4942
LucasLechevestrier <101550799+LucasLechevestrier@users.noreply.github.com>
2022-05-07 10:55:16 +0200

    add JsonReducerController.java

---
+++ src/main/java/pl/put/poznan/transformer/rest/JsonReducerController.java
@@ -0,0 +1,45 @@
+package pl.put.poznan.transformer.rest;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.transformer.logic.JsonFormatter;
+import pl.put.poznan.transformer.logic.JsonReader;
+import pl.put.poznan.transformer.logic.JsonReducer;
+
+
+
+@RestController
+@RequestMapping("/reduce/{text}")
+public class JsonReducerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(JsonReducerController.class);
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public JsonNode get(@PathVariable String text) {
+
+        // log the parameters
+        logger.debug(text);
+        JsonFormatter formatter = new JsonReader(text);
+        JsonReducer reducer = new JsonReducer(formatter);
+
+        return reducer.parse();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public JsonNode post(@PathVariable String text) {
+
+        // log the parameters
+        logger.debug(text);
+
+        JsonFormatter formatter = new JsonReader(text);
+        JsonReducer reducer = new JsonReducer(formatter);
+
+        return reducer.parse();
+    }
+
+
+
+}
+
